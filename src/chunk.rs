0cb1f9081e96e302a0d3fc305f4caf463436434a
Karol Moroz <karol@moroz.dev>
2024-08-27 01:45:55 +0800

    Add constant opcode

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,25 +1,34 @@
-use std::process::exit;
-
+use crate::value;
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
 #[derive(FromPrimitive)]
 #[repr(u8)]
 pub enum OpCode {
+    Constant,
     Return,
 }
 
 pub struct Chunk {
     code: Vec<u8>,
+    constants: Vec<value::Value>,
 }
 
 impl Chunk {
     pub fn new() -> Self {
-        Self { code: vec![] }
+        Self {
+            code: vec![],
+            constants: vec![],
+        }
     }
 
     pub fn write_chunk(&mut self, byte: u8) {
         self.code.push(byte);
+    }
+
+    pub fn add_constant(&mut self, value: value::Value) -> u8 {
+        self.constants.push(value);
+        return (self.constants.len() - 1) as u8;
     }
 
     pub fn disassemble_chunk(&self, name: &str) {
@@ -40,6 +49,9 @@ impl Chunk {
             Some(OpCode::Return) => {
                 return self.simple_instruction("OP_RETURN", offset);
             }
+            Some(OpCode::Constant) => {
+                return self.constant_instruction("OP_CONSTANT", offset);
+            }
             None => {
                 println!("Unknown opcode {instruction}");
                 return offset + 1;
@@ -50,5 +62,12 @@ impl Chunk {
     fn simple_instruction(&self, name: &str, offset: usize) -> usize {
         println!("{}", name);
         return offset + 1;
+    }
+
+    fn constant_instruction(&self, name: &str, offset: usize) -> usize {
+        let constant = self.code.get(offset + 1).unwrap();
+        let value = self.constants.get(*constant as usize).unwrap();
+        println!("{:<16} {:4} '{value}'", name, constant);
+        return offset + 2;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod chunk;
+mod value;
+
+use chunk::{Chunk, OpCode};
 
 fn main() {
-    let mut chunk = chunk::Chunk::new();
-    chunk.write_chunk(chunk::OpCode::Return as u8);
+    let mut chunk = Chunk::new();
+
+    let constant = chunk.add_constant(1.2);
+    chunk.write_chunk(OpCode::Constant as u8);
+    chunk.write_chunk(constant);
+    chunk.write_chunk(OpCode::Return as u8);
     chunk.disassemble_chunk("test chunk");
 }
